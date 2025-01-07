10230df448d49c93fb064557335bb9c764220783
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2025-01-07 15:22:10 -0300

    Fixing barrel imports

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -4,3 +4,8 @@ export * from "./dtos/auth/register.dto";
 export * from "./entities/user.entity";
 export * from "./errors/custom.error";
 export * from "./repositories/auth.repository";
+export * from "./use-case/auth/login-user";
+export * from "./use-case/auth/register-user";
+export * from "./use-case/auth/validate-mail";
+
+export * from "./repositories/auth.repository";
