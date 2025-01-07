9d3fd459f2a9f8037ea180b035171498a2b1e716
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2025-01-07 15:21:39 -0300

    Adding mail config envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,4 +4,10 @@ import { get } from "env-var";
 export const envs = {
   PORT: get("PORT").required().asPortNumber(),
   SEED: get("SEED").required().asString(),
+
+  MAIL_SERVICE: get("MAIL_SERVICE").required().asString(),
+  MAIL: get("MAIL").required().asString(),
+  SECRET: get("SECRET").required().asString(),
+
+  WEBSERVICE_URL: get("WEBSERVICE_URL").required().asString(),
 };
