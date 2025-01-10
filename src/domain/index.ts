c611d0156bfb04eb1cbda6f2dfce4e46b6ab3e28
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2025-01-10 15:34:44 -0300

    fixing imports

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,11 +1,14 @@
-export * from "./datasources/auth.datasources";
+export * from "./datasources/auth.datasource";
+export * from "./datasources/category.datasource";
 export * from "./dtos/auth/login.dto";
 export * from "./dtos/auth/register.dto";
+export * from "./dtos/category/create-category.dto";
 export * from "./entities/user.entity";
 export * from "./errors/custom.error";
 export * from "./repositories/auth.repository";
+export * from "./repositories/auth.repository";
+export * from "./repositories/category.repository";
 export * from "./use-case/auth/login-user";
 export * from "./use-case/auth/register-user";
 export * from "./use-case/auth/validate-mail";
-
-export * from "./repositories/auth.repository";
+export * from "./use-case/category/get-category";
--- src/infraestructure/index.ts
+++ src/infraestructure/index.ts
@@ -1,2 +1,4 @@
 export * from "./datasources/auth.datasource.impl";
+export * from "./datasources/category.datasource.impl";
+export * from "./repositories/category.repository.impl";
 export * from "./repositories/auth.repository.impl";
