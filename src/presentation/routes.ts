a23c8d578ea4d0cdc5f107d98902f7f9d5d053d0
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2025-01-10 15:43:44 -0300

    Adding the routes for product and category

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { AuthRoutes } from "./auth/routes";
+import { CategoryRoutes } from "./category/routes";
+import { ProductRoutes } from "./product/routes";
 
 export class AppRotes {
   static get routes(): Router {
@@ -7,6 +9,8 @@ export class AppRotes {
 
     //* Routes
     router.use("/api/v1/auth", AuthRoutes.routes);
+    router.use("/api/v1/category", CategoryRoutes.routes);
+    router.use("/api/v1/products", ProductRoutes.routes);
 
     return router;
   }
